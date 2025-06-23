ea60bbdb524c8cb679a3327dafb43804f0c8af61
Slowrabbets <kadencooper608@gmail.com>
2025-06-23 14:21:26 -0600

    Add enviromental variables

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DB_USER = "user"
-DB_PASS = "password"
-DB_NAME = "taskdb"
-DB_HOST = "db"
-DB_PORT = "5432"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
