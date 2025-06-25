a7af2cfd302e4aa4cca161e26f38aa8a74a63bcc
Slowrabbets <kadencooper608@gmail.com>
2025-06-25 10:49:51 -0600

    Fix port

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = int(os.getenv("DB_PORT", "5432").strip().replace('"', ''))
 
 # Construct the database URL using the environment variables
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
